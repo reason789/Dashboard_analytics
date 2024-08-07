e1b0f2bd190b1c6f32e73ec32a25541de32d99b8
reason789 <rahadul134@gmail.com>
2024-08-07 19:53:27 +0600

    design added

--- app/page.js
+++ app/page.js
@@ -8,17 +8,17 @@ export default function Home() {
       <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
         Google Analysis Dashboard
       </h1>
-      <div className="bg-white shadow-md rounded-lg p-6 mb-6">
+      <div className="max-w-3xl mx-auto bg-white shadow-md rounded-lg p-6 mb-6">
         <h3 className="text-2xl font-semibold text-gray-700 mb-4">
           IP Address
         </h3>
         <GetIpAddress />
       </div>
-      <div className="bg-white shadow-md rounded-lg p-6 mb-6">
+      <div className="max-w-3xl mx-auto bg-white shadow-md rounded-lg p-6 mb-6">
         <h3 className="text-2xl font-semibold text-gray-700 mb-4">Location</h3>
         <Location />
       </div>
-      <div className="bg-white shadow-md rounded-lg p-6">
+      <div className="max-w-3xl mx-auto bg-white shadow-md rounded-lg p-6">
         <h3 className="text-2xl font-semibold text-gray-700 mb-4">
           Device Information
         </h3>
